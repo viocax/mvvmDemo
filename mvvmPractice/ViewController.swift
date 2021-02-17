a47c16e84b380dd8bf23518fe60e4dae5980b8e9
Jie liang Huang <angelica812860@live.com>
2021-02-17 21:59:24 +0800

    Initial Commit

---
+++ mvvmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvmPractice
+//
+//  Created by Jie liang Huang on 2021/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
